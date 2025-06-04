e16b11e8051d2cd2c60cfde52ac23f6123c9b066
debiasio001 <ingriddebiasio2020@gmail.com>
2025-06-03 21:50:44 -0300

    aula01 greenfood

---
+++ Greenfoot/Aula01/MundoAbelha/MyWorld.java
@@ -0,0 +1,54 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class MyWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MyWorld extends World
+{
+
+    /**
+     * Constructor for objects of class MyWorld.
+     * 
+     */
+    public MyWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(800, 600, 1); 
+        prepare();
+    }
+    
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Aranha aranha = new Aranha();
+        addObject(aranha,39,29);
+        aranha.setLocation(25,31);
+        aranha.setLocation(33,29);
+        aranha.setLocation(30,30);
+        Mosca mosca = new Mosca();
+        addObject(mosca,42,332);
+        mosca.setLocation(42,332);
+        Abelha abelha = new Abelha();
+        addObject(abelha,41,155);
+        mosca.setLocation(25,315);
+        mosca.setLocation(29,325);
+        Mosca mosca2 = new Mosca();
+        addObject(mosca2,449,90);
+        Mosca mosca3 = new Mosca();
+        addObject(mosca3,396,332);
+        Mosca mosca4 = new Mosca();
+        addObject(mosca4,107,271);
+        mosca4.setLocation(92,264);
+        mosca3.setLocation(96,337);
+        abelha.setLocation(49,143);
+        mosca4.setLocation(31,273);
+        mosca.setLocation(33,378);
+        mosca2.setLocation(103,266);
+    }
+}
